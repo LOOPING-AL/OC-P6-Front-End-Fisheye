1b354216b471a04a544138b8babd30749bf4186f
Alex <gitlab.bec5f@aleeas.com>
2023-01-16 20:42:51 +0100

    start to import and use data

--- assets/scripts/factories/photographer.js
+++ assets/scripts/factories/photographer.js
@@ -1,17 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/images/SamplePhotos/PhotographersID/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    const h4 = document.createElement("h4");
+    h4.textContent = tagline;
+    const h5 = document.createElement("h5");
+    h5.textContent = `${price} €/jour`;
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(h4);
+    article.appendChild(h5);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
